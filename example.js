106313a23f5c292a12d81747bf89eb61f2f7762f
rileygoerg <rgoerg@usf.edu>
2024-06-16 23:33:20 -0400

    Implement data loading and DOM manipulation with D3.js.

--- example.js
+++ example.js
@@ -1,15 +1,14 @@
 // U14123683
 // Logic for Data Loading and Display
-function loadData(){
-    d3.csv("../data/purchase_orders.csv").then(
+function main(){
+    d3.csv('data/purchase_orders.csv').then(
         function (d) {
             for (let index = 0; index < d.length; index++) {
                 const element = d[index];
                 d3.select("body")
                 .append("ul")
                 .append("li")
-                .text("Customer Name: " + d.customerName + ", Order ID: " + d.orderId + ", Purchase Amount: " + d.purchaseAmount)
-                
+                .text(element.customerName + " - #" + element.orderId + ": $" + element.purchaseAmount)
             }
         }
 
