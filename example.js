586727a8eb2322b72b98ac7b46b2f41c96e23ccc
rileygoerg <rgoerg@usf.edu>
2024-06-16 23:49:24 -0400

    Finalize data display functionality.

--- example.js
+++ example.js
@@ -1,13 +1,13 @@
 // U14123683
 // Logic for Data Loading and Display
 function main(){
-    d3.csv('data/purchase_orders.csv').then(
+    d3.dsv(',','data/purchase_orders.csv', d3.autoType).then( // loads data
         function (d) {
-            for (let index = 0; index < d.length; index++) {
+            for (let index = 0; index < d.length; index++) { // for loop to obtain each row of data
                 const element = d[index];
                 d3.select("body")
                 .append("ul")
-                .append("li")
+                .append("li") // appends each row to a list item in an unordered list
                 .text(element.customerName + " - #" + element.orderId + ": $" + element.purchaseAmount)
             }
         }
